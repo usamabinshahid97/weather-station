5852b4f1aa92070ed7aa8e02d5b14a8d9a03606e
Usama Bin Shahid <usamabinshahid97>
2020-01-20 23:18:04 +0500

    Weather Station completed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store'
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
